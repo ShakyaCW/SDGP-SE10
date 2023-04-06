b600f9b9c646e726d30c4effab15cdea7c743ac5
ShakyaCW <108785724+ShakyaCW@users.noreply.github.com>
2023-04-06 11:51:50 +0530

    minor changes to js

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 
 const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+console.log(process.env);
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
